d15ace0458d50bdb35babdbf8c702d9c7e0bfd37
MachineLearningAmateur <personalusage123@gmail.com>
2021-02-23 23:44:56 -0800

    fixed typos

--- src/Mob.cpp
+++ src/Mob.cpp
@@ -14,7 +14,7 @@ Mob::~Mob()
 void Mob::attack(Basic_Unit* target)
 {
 	target->decrease_health(attackValue);
-	std::cout << name << " has dealt " << std::to_string(attackValue) << " danage to " << target->get_name() << ". " << target->get_name() << " has " << std::to_string(target->get_health()) << "/" << std::to_string(healthCap) << " health remaining." << std::endl; 
+	std::cout << name << " has dealt " << std::to_string(attackValue) << " damage to " << target->get_name() << ". " << target->get_name() << " has " << std::to_string(target->get_health()) << "/" << std::to_string(healthCap) << " health remaining." << std::endl; 
 }
 
 void Mob::recover()
@@ -42,7 +42,7 @@ void Mob::recover()
 void Mob::special(Basic_Unit* target)
 {
 	target->decrease_health(attackValue+50);
-	std::cout << name << "casts Special on " << target->get_name() << ". " << name << " deals " << std::to_string(attackValue) << " damage to "
+	std::cout << name << " casts Special on " << target->get_name() << ". " << name << " deals " << std::to_string(attackValue) << " damage to "
     << target->get_name() << ". " << target->get_name() << " has " << std::to_string(target->get_health()) << "/" << std::to_string(healthCap) << " health remaining." <<
     std::endl;
 }
